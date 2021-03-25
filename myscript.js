7780026c4da522f3ca4e15dcb4535acd59ab6160
Sergio <sergio.marretta@libero.it>
2021-03-25 18:04:21 +0100

    carousel next & prev

--- myscript.js
+++ myscript.js
@@ -1,15 +1,30 @@
 var app = new Vue (
-{
+  {
   el: "#app",
- data: {
-images: [
-"https://www.globalpsa.com/wp-content/uploads/190128-Palermo-Sicily-Italy-IMG00.jpg",
-"https://www.costacrociere.it/content/dam/costa/costa-magazine/article-images/palermo-beaches/9-spiagge-palermo-694x390.jpg.image.694.390.low.jpg",
-"https://media-cdn.sygictraveldata.com/media/800x600/612664395a40232133447d33247d383737363036333638"
-]
+  data: {
+    images: [
+    "https://www.globalpsa.com/wp-content/uploads/190128-Palermo-Sicily-Italy-IMG00.jpg",
+    "https://www.costacrociere.it/content/dam/costa/costa-magazine/article-images/palermo-beaches/9-spiagge-palermo-694x390.jpg.image.694.390.low.jpg",
+    "https://media-cdn.sygictraveldata.com/media/800x600/612664395a40232133447d33247d383737363036333638"],
+    imgIndex: 0
+  },
+  methods: {
+  nextImg: function(){
+  this.imgIndex++;
 
+  if (this.imgIndex == 3) {
+    this.imgIndex = 0;
+  }
+
+},
+ prevImg: function(){
+this.imgIndex--;
+if (this.imgIndex < 0) {
+  this.imgIndex = 2;
+}
  }
 
-}
+  }
 
+}
 );
