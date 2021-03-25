63a094363f7f406e1f90be68771e0fd1a6f9d1cd
Sergio <sergio.marretta@libero.it>
2021-03-25 17:25:06 +0100

    carousel start

---
+++ myscript.js
@@ -0,0 +1,15 @@
+var app = new Vue (
+{
+  el: "#app",
+ data: {
+images: [
+"https://www.globalpsa.com/wp-content/uploads/190128-Palermo-Sicily-Italy-IMG00.jpg",
+"https://www.costacrociere.it/content/dam/costa/costa-magazine/article-images/palermo-beaches/9-spiagge-palermo-694x390.jpg.image.694.390.low.jpg",
+"https://media-cdn.sygictraveldata.com/media/800x600/612664395a40232133447d33247d383737363036333638"
+]
+
+ }
+
+}
+
+);
